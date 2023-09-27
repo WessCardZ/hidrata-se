215e22cb2b6ab7ff0c349516d520e84555d8715b
Wesley <wesleyofficial@hotmail.com>
2023-09-27 13:33:41 -0300

    iniciando aba de Perfil

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Perfil from './src/perfil/Pefil'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Perfil />
       <StatusBar style="auto" />
     </View>
   );
