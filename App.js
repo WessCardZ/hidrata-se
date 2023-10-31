cde7a87e91708fd3c6924fdfa9bdf73e56cec6d3
Wesley <wesleyofficial@hotmail.com>
2023-10-31 16:42:41 -0300

    Historico sendo feito com API Back End

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Perfil from './src/perfil/Pefil';
-import Historico from './src/historico/Histrico';
+import Telahistorico from './src/historico/Histrico';
 import Lembretes from './src/perfil/lembretes/lembretes';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,13 +11,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Perfil' >
-        <Stack.Screen name='Perfil' component={Perfil} options={{ headerShown: false }} />
-        <Stack.Screen name='Lembretes' component={Lembretes} options={{ title: 'Lembretes' }} />
-        <Stack.Screen name='sonsevibracao' component={sonsevibracao} options={{ title: 'Sons e vibração' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName='Perfil' >
+    //     <Stack.Screen name='Perfil' component={Perfil} options={{ headerShown: false }} />
+    //     <Stack.Screen name='Lembretes' component={Lembretes} options={{ title: 'Lembretes' }} />
+    //     <Stack.Screen name='sonsevibracao' component={sonsevibracao} options={{ title: 'Sons e vibração' }} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <Telahistorico />
   );
 }
 
