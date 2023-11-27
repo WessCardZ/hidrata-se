62d31e434a86d85966de6f519e94f2c880d482f2
Jeremias Verissimo Gomes <>
2023-11-27 16:38:31 -0300

    Cadastroinicio feito e estilo 70% completo

---
+++ src/cadastro/cadastroinicio.js
@@ -0,0 +1,32 @@
+import { View, Text, Pressable,} from 'react-native';
+import { Button } from 'react-native-paper';
+import { useFonts, Montserrat_700Bold, Montserrat_400Regular, Montserrat_600SemiBold, Montserrat_500Medium } from "@expo-google-fonts/montserrat";
+import { StyleSheet } from 'react-native';
+import style from './style.js'
+
+
+
+function Cadastroinicio(){
+    let [fontsLoaded, fontError] = useFonts({
+        Montserrat_700Bold
+    });
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+    return (
+        <View style={style.container}>
+            <Text style={style.texto}> Vamos começar </Text>
+
+        <View style={style.continainer2}>
+        <Button style={style.Button} mode="contained" buttonColor='#00A1FF' onPress={() => console.log('Pressed')}>
+        Proximo
+        </Button>
+        </View>
+        </View>
+
+    )
+  }
+
+
+  export default Cadastroinicio
---
+++ src/cadastro/style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#6CC2F2',
+        alignItems: 'center',
+        paddingTop: 305,
+    },
+    texto:{
+        color: '#2D4F63',
+        fontSize: 36,
+        fontFamily: 'Montserrat_700Bold',
+
+    },
+    continainer2:{
+        paddingTop: 47,
+    },
+    Button:{
+        paddingHorizontal: 125,
+        borderRadius: 15,
+        
+    }
+});
+
+
+export default style;
