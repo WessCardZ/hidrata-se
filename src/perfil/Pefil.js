3113bf03bb807a347d0d98992aae2d15ef4704ad
WessCardZ <wesleyofficial@hotmail.com>
2023-12-09 12:22:19 -0300

    Nova pasta para ModalPeso

--- src/perfil/Pefil.js
+++ src/perfil/Pefil.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 import GoogleFonts from "../components/GoogleFonts";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Modalhorario from "./horarios/modalHorario";
+import ModalPeso from "./peso/modalPeso";
 
 export default function TelaPerfil() {
     const [isLoading, setLoading] = useState(true)
@@ -127,95 +128,6 @@ const Lista = ({ titulo, subtitulo, nomeIcone, onPress }) => {
                 </View>
             </View>
         </Pressable>
-    );
-}
-
-
-const ModalPeso = ({ modalPesoVisible, setModalPesoVisible }) => {
-    const [peso, setPeso] = useState('');
-    const [meta, setMeta] = useState('');
-    const [acordar, setAcordar] = useState('');
-    const [dormir, setDormir] = useState('');
-
-    useEffect(() => {
-        getUsuarioConfig();
-    }, []);
-
-    const getUsuarioConfig = async () => {
-        const userId = await AsyncStorage.getItem('userId');
-        try {
-            const response = await fetch(`https://aguaprojeto.onrender.com/usuarioconfig/usuario/${userId}`);
-            const json = await response.json();
-
-            setPeso(json.pesoAtual);
-            setMeta(json.metaDiaria);
-
-            setAcordar(formatarHora(json.horarioAcordar));
-            setDormir(formatarHora(json.horarioDormir));
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const formatarHora = (value) => {
-        const [horas, minutos] = value.split(':');
-        return `${horas}${minutos}`;
-    };
-
-    const putPeso = async () => {
-        const userId = await AsyncStorage.getItem('userId');
-        try {
-            const response = await fetch(`https://aguaprojeto.onrender.com/usuarioconfig/usuario/${userId}`, {
-                method: 'PUT',
-                headers: { 'Content-type': 'application/json' },
-                body: JSON.stringify({
-                    pesoAtual: peso,
-                    horarioAcordar: acordar,
-                    horarioDormir: dormir,
-                    metaDiaria: meta,
-                }),
-            });
-            if (response.status === 204) {
-                setModalPesoVisible(false);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    return (
-        <KeyboardAvoidingView>
-            <Modal animationType="fade" transparent={true} visible={modalPesoVisible}>
-                <View style={style.fundoModa}>
-                    <View style={style.modal}>
-                        <View style={{ alignItems: 'center' }}>
-                            <Text style={style.tituloModal}>Peso</Text>
-                        </View>
-
-                        <View style={style.inputModal}>
-                            <TextInput
-                                keyboardType="numeric"
-                                style={style.textoInputModal}
-                                maxLength={5}
-                                value={peso}
-                                placeholder="60"
-                                onChangeText={(text) => setPeso(text)}
-                            />
-                            <Text style={style.Kg}>Kg</Text>
-                        </View>
-
-                        <Pressable style={style.botaoModal}>
-                            <Pressable onPress={() => setModalPesoVisible(false)} style={style.containerBotaoModal}>
-                                <Text style={style.textoBotaoModal}>Cancelar</Text>
-                            </Pressable>
-                            <Pressable onPress={putPeso} style={style.containerBotaoModal2}>
-                                <Text style={style.textoBotaoModal}>Salvar</Text>
-                            </Pressable>
-                        </Pressable>
-                    </View>
-                </View>
-            </Modal>
-        </KeyboardAvoidingView>
     );
 }
 
