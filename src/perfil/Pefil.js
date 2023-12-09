188655ea24f973bbd772581357362096565d2f48
WessCardZ <wesleyofficial@hotmail.com>
2023-12-09 12:18:30 -0300

    Pasta para ModalHorario

--- src/perfil/Pefil.js
+++ src/perfil/Pefil.js
@@ -5,6 +5,7 @@ import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import GoogleFonts from "../components/GoogleFonts";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Modalhorario from "./horarios/modalHorario";
 
 export default function TelaPerfil() {
     const [isLoading, setLoading] = useState(true)
@@ -129,118 +130,6 @@ const Lista = ({ titulo, subtitulo, nomeIcone, onPress }) => {
     );
 }
 
-const Modalhorario = ({ modalVisible, setModalVisible }) => {
-    const [peso, setPeso] = useState('');
-    const [meta, setMeta] = useState('');
-    const [acordar, setAcordar] = useState('');
-    const [dormir, setDormir] = useState('');
-
-    useEffect(() => {
-        getHorario();
-    }, []);
-
-    const getHorario = async () => {
-        const userId = await AsyncStorage.getItem('userId');
-        try {
-            const response = await fetch(`https://aguaprojeto.onrender.com/usuarioconfig/usuario/${userId}`);
-            const json = await response.json();
-
-            setAcordar(json.horarioAcordar);
-            setDormir(json.horarioDormir);
-            setPeso(json.pesoAtual);
-            setMeta(json.metaDiaria);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const handleTrocaInput = (text, setState) => {
-        const numericValue = text.replace(/[^0-9]/g, '');
-        const limiteNumerico = numericValue.slice(0, 4);
-        const valorFormatado = formatarHora(limiteNumerico);
-
-        setState(valorFormatado);
-    };
-
-    const formatarHora = (value) => {
-        if (value.length <= 2) {
-            return value;
-        } else {
-            const hora = value.slice(0, 2);
-            const minutos = value.slice(2, 4);
-            return `${hora}:${minutos}`;
-        }
-    };
-
-    const putHorarios = async () => {
-        const userId = await AsyncStorage.getItem('userId');
-        try {
-            const response = await fetch(`https://aguaprojeto.onrender.com/usuarioconfig/usuario/${userId}`, {
-                method: 'PUT',
-                headers: { 'Content-type': 'application/json' },
-                body: JSON.stringify({
-                    pesoAtual: peso,
-                    horarioAcordar: acordar,
-                    horarioDormir: dormir,
-                    metaDiaria: meta,
-                }),
-            });
-
-            if (response.status === 204) {
-                setModalVisible(false);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    return (
-        <KeyboardAvoidingView>
-            <Modal animationType="fade" transparent={true} visible={modalVisible}>
-                <View style={style.fundoModa}>
-                    <View style={style.modal}>
-                        <View style={{ alignItems: 'center' }}>
-                            <Text style={style.tituloModal}>Horários</Text>
-                        </View>
-
-                        <View>
-                            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-                                <Text style={style.textoHora}>Acordar</Text>
-                                <Text style={style.textoHora}>Dormir</Text>
-                            </View>
-
-                            <View style={style.inputs}>
-                                <TextInput
-                                    keyboardType="numeric"
-                                    style={style.textoInput}
-                                    maxLength={5}
-                                    value={acordar}
-                                    onChangeText={(text) => handleTrocaInput(text, setAcordar)}
-                                />
-                                <TextInput
-                                    keyboardType="numeric"
-                                    style={style.textoInput}
-                                    maxLength={5}
-                                    value={dormir}
-                                    onChangeText={(text) => handleTrocaInput(text, setDormir)}
-                                />
-                            </View>
-                        </View>
-
-                        <Pressable style={style.botaoModal}>
-                            <Pressable onPress={() => setModalVisible(false)} style={style.containerBotaoModal}>
-                                <Text style={style.textoBotaoModal}>Cancelar</Text>
-                            </Pressable>
-                            <Pressable onPress={putHorarios} style={style.containerBotaoModal2}>
-                                <Text style={style.textoBotaoModal}>Salvar</Text>
-                            </Pressable>
-                        </Pressable>
-                    </View>
-                </View>
-            </Modal>
-        </KeyboardAvoidingView>
-    );
-};
 
 const ModalPeso = ({ modalPesoVisible, setModalPesoVisible }) => {
     const [peso, setPeso] = useState('');
